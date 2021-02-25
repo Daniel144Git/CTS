63c64be32743aceff9bec7c24ef3c56c35fe3809
Daniel144Gitgit config --global user.email florinmusoiu@gmail.com <Daniel@192.168.0.94>
2021-02-25 07:28:28 -0800

    p

--- eclipse-workspace/workspace/seminar_cts/src/seminar_cts/Main.java
+++ eclipse-workspace/workspace/seminar_cts/src/seminar_cts/Main.java
@@ -1,9 +1,25 @@
 package seminar_cts;
 
+import clase.Zookeeper;
+import clase.Animal;
+import clase.Giraffe;
+import clase.Zebra;
+import clase.Zoo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Zoo z=new Zoo();
+		Giraffe giraffe = new giraffe("Giraffe");
+		Giraffe Giraffe2 = new Giraffe("Giraffe2");
+		
+		z.addAnimal(giraffe);
+		z.addAnimal(giraffe2);
+		
+		z.feedAllAnimals();
+		
+		Zebra z1
 
 	}
 
